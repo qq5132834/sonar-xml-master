f18d5cd31764361e1bd40b2e65ae5d8cbc7c526e
qq5132834 <513283439@qq.com>
2022-08-11 17:01:39 +0800

    获取maven项目中pom.xml关系依赖

--- sonar-xml-plugin/src/test/java/org/sonar/plugins/xml/checks/maven/DependencyWithSystemScopeCheckTest.java
+++ sonar-xml-plugin/src/test/java/org/sonar/plugins/xml/checks/maven/DependencyWithSystemScopeCheckTest.java
@@ -20,7 +20,28 @@
 package org.sonar.plugins.xml.checks.maven;
 
 import org.junit.jupiter.api.Test;
+import org.sonar.api.batch.fs.InputFile;
+import org.sonar.api.batch.fs.internal.DefaultInputFile;
+import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
+import org.sonar.api.batch.sensor.internal.SensorContextTester;
+import org.sonarsource.analyzer.commons.xml.XmlFile;
+import org.sonarsource.analyzer.commons.xml.XmlTextRange;
+import org.sonarsource.analyzer.commons.xml.checks.SonarXmlCheck;
 import org.sonarsource.analyzer.commons.xml.checks.SonarXmlCheckVerifier;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class DependencyWithSystemScopeCheckTest {
   @Test
@@ -28,6 +49,62 @@ class DependencyWithSystemScopeCheckTest {
     DependencyWithSystemScopeCheck check = new DependencyWithSystemScopeCheck();
     SonarXmlCheckVerifier.verifyIssues("pom.xml", check);
     SonarXmlCheckVerifier.verifyNoIssue("../irrelevant.xml", check);
+  }
+
+  @Test
+  void createVerifier() {
+    File file = new File("src\\test\\resources\\checks\\DependencyWithSystemScopeCheck\\pom.xml");
+
+    String filePath = file.getPath();
+    String content;
+    try (Stream<String> lines = Files.lines(file.toPath())) {
+      content = lines.collect(Collectors.joining("\n"));
+    } catch (IOException e) {
+      throw new IllegalStateException(String.format("Unable to load content of file %s", filePath), e);
+    }
+
+    DefaultInputFile defaultInputFile = TestInputFileBuilder.create("", filePath)
+            .setType(InputFile.Type.MAIN)
+            .initMetadata(content)
+            .setLanguage("xml")
+            .setCharset(StandardCharsets.UTF_8)
+            .build();
+
+
+    XmlFile xmlFile;
+    try {
+      xmlFile = XmlFile.create(defaultInputFile);
+    } catch (Exception e) {
+      throw new IllegalStateException(String.format("Unable to scan xml file %s", filePath), e);
+    }
+    Node node = xmlFile.getNamespaceUnawareDocument();
+
+    XPathExpression dependencyExpression = getXPathExpression("//dependencies/dependency");
+    List<Node> nodeList = XmlFile.asList(evaluate(dependencyExpression, node));
+
+    if (nodeList != null) {
+      nodeList.stream().forEach(n -> {
+        XmlTextRange xmlTextRange = XmlFile.nodeLocation(n);
+        System.out.println("依赖规则:" + xmlTextRange.getStartLine() + "," + xmlTextRange.getEndLine());
+      });
+    }
+  }
+
+  public XPathExpression getXPathExpression(String expression) {
+    try {
+      XPath xpath = XPathFactory.newInstance().newXPath();
+      return xpath.compile(expression);
+    } catch (XPathExpressionException e) {
+        return null;
+    }
+  }
+
+  public NodeList evaluate(XPathExpression expression, Node node) {
+    try {
+      return (NodeList) expression.evaluate(node, XPathConstants.NODESET);
+    } catch (XPathExpressionException e) {
+      return null;
+    }
   }
 
 }
